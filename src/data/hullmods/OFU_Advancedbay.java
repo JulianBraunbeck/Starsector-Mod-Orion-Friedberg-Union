2b5396affabb338d910aaa7f931ae4997a81de33
Julian BRaunbeck <juliangeb@gmx.de>
2025-08-12 20:20:37 +0200

    second patch
    Added blueprints
    
    Buffed Hector
    
    Buffed Bomber command

--- src/data/hullmods/OFU_Advancedbay.java
+++ src/data/hullmods/OFU_Advancedbay.java
@@ -8,27 +8,30 @@ import com.fs.starfarer.api.combat.ShipAPI;
 
 public class OFU_Advancedbay extends BaseHullMod {
 
-    public static float FIGHTER_SPEED_BONUS = 15f;
-    public static float HULL_BONUS = 20f;
+    public static float FLUX_RESISTANCE = 75f;
+    public static float FIGHTER_SPEED_BONUS = 25f;
+    public static float HULL_BONUS = 150f;
 
-    public static float MANEUVER_BONUS = 25f;
+    public static float MANEUVER_BONUS = 35f;
 
-    public static float ARMOR_BONUS = 20f;
+    public static float ARMOR_BONUS = 50f;
 
 
     public void applyEffectsToFighterSpawnedByShip(ShipAPI fighter, ShipAPI ship, String id) {
 
         MutableShipStatsAPI stats = fighter.getMutableStats();
 
+        stats.getEmpDamageTakenMult().modifyMult(id, 1f - FLUX_RESISTANCE * 0.01f);
+
         stats.getMaxSpeed().modifyPercent(id, FIGHTER_SPEED_BONUS);
-        stats.getHullBonus().modifyPercent(id, HULL_BONUS);
+        stats.getHullBonus().modifyFlat(id, HULL_BONUS);
 
         stats.getAcceleration().modifyPercent(id, MANEUVER_BONUS * 2f);
         stats.getDeceleration().modifyPercent(id, MANEUVER_BONUS);
         stats.getTurnAcceleration().modifyPercent(id, MANEUVER_BONUS * 2f);
         stats.getMaxTurnRate().modifyPercent(id, MANEUVER_BONUS);
 
-        stats.getArmorBonus().modifyPercent(id, ARMOR_BONUS);
+        stats.getArmorBonus().modifyFlat(id, ARMOR_BONUS);
 
 
     }
@@ -36,8 +39,9 @@ public class OFU_Advancedbay extends BaseHullMod {
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0) return "" + (int) (FIGHTER_SPEED_BONUS) + "%";
         if (index == 1) return "" + (int) (MANEUVER_BONUS) + "%";
-        if (index == 2) return "" + (int) (HULL_BONUS) + "%";
-        if (index == 3) return "" + (int) (ARMOR_BONUS) + "%";
+        if (index == 2) return "" + (int) (HULL_BONUS);
+        if (index == 3) return "" + (int) (ARMOR_BONUS);
+        if (index == 4) return "" + (int) (FLUX_RESISTANCE) + "%";
         return null;
     }
 
